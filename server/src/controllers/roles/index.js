5704f80d11a61cd98c376ca785eb89aeb94584ce
Drzala3312 <dhruvizalarajesh@gmail.com>
2021-01-15 23:45:02 +0530

    role issue

--- server/src/controllers/roles/index.js
+++ server/src/controllers/roles/index.js
@@ -19,10 +19,7 @@ routes.push({
     middleware: (req, res, next) => {
         models.roles
             .findAll({
-                attributes: [
-                    'rid',
-                    'name',
-                ],
+                attributes: ['rid', 'name'],
             })
             .then((data) => {
                 const resObj = data.map((permission) => {
@@ -31,7 +28,7 @@ routes.push({
                         {},
                         {
                             rid: permission.rid,
-                            name: permission.name
+                            name: permission.name,
                         }
                     );
                 });
@@ -56,7 +53,8 @@ routes.push({
         // object
         const form = {
             rid: req.body.rid,
-            name: req.body.name
+            name: req.body.name,
+            permissions: req.body.permissions ? req.body.permissions : null,
         };
 
         // create record
@@ -64,6 +62,58 @@ routes.push({
             .create(form)
             .then((data) => {
                 res.json(data);
+                if (data.length > 0) {
+
+                    // open transaction
+                    return models.sequelize
+                        .transaction((t) => {
+                            // create booking
+                            return models.roles.create(form).then((data) => {
+                                // map rooms and create one by one
+                                return Promise.map(form.permissions, (pid) => {
+                                    return models.sequelize.query(
+                                        'INSERT INTO role_permission (rid, pid) VALUES (:rid, :pid)',
+                                        {
+                                            transaction: t,
+                                            replacements: {
+                                                rid: data.id,
+                                                pid: pid,
+                                            },
+                                            type: Sequelize.QueryTypes.INSERT,
+                                        }
+                                    );
+                                }).then(() => {
+                                    return data;
+                                });
+                            });
+                        })
+                        .then((data) => {
+                            res.json(data);
+                            return next();
+                        })
+                        .catch((err) => {
+                            res.status(400);
+                            if (err.name === 'SequelizeValidationError') {
+                                res.json({
+                                    errors: err.errors,
+                                    name: err.name,
+                                });
+                            } else {
+                                res.json({
+                                    errors: [
+                                        {
+                                            message: err.message,
+                                        },
+                                    ],
+                                });
+                            }
+
+                            return next();
+                        });
+                } else {
+                    throw Error('Select at least one room');
+                }
+
                 return next();
             })
             .catch((err) => {
@@ -80,6 +130,82 @@ routes.push({
             });
     },
 });
+
+
+
+
+routes.push({
+    meta: {
+        name: 'permissionUpdate',
+        method: 'PUT',
+        paths: ['/permission/:id'],
+    },
+    middleware: (req, res, next) => {
+        const id = req.params.id;
+        // object
+        const form = {
+            rid: req.body.rid,
+            name: req.body.name,
+            permissions: req.body.permissions ? req.body.permissions : null,
+        };
+
+        console.log(form);
+        console.log(models.permission);
+        // update record
+        models.permission
+            .findOne({
+                where: {
+                    id: {
+                        [Sequelize.Op.eq]: req.params.id,
+                    },
+                },
+            })
+            .then((data) => {
+                return data.update(form).then((updated) => {
+                    return models.sequelize.query('DELETE FROM role_permission WHERE rid = :rid', {
+                        transaction: t,
+                        replacements: {
+                            booking_id: updated.id
+                        },
+                        type: Sequelize.QueryTypes.DELETE
+                    }).then(() => {
+                        // map pid and create one by one
+                        return Promise.map(form.permissions, (pid) => {
+                            return models.sequelize.query('INSERT INTO role_permission (rid, pid) VALUES (:rid, :pid)', {
+                                transaction: t,
+                                replacements: {
+                                    rid: data.id,
+                                    pid: pid,
+                                },
+                                type: Sequelize.QueryTypes.INSERT
+                            });
+                        }).then(() => {
+                            return updated;
+                        });
+                    });
+                });
+            })
+            .then((data) => {
+                res.json(data);
+                return next();
+            })
+            .catch((err) => {
+                console.log(err)
+                if (err.name === 'SequelizeValidationError') {
+                    res.status(400);
+                    res.json({
+                        errors: err.errors,
+                        name: err.name,
+                    });
+                } else {
+                    res.json(err);
+                }
+
+                return next();
+            });
+    },
+});
+
 
 /**
  * @action delete
@@ -101,7 +227,7 @@ routes.push({
                     },
                     pid: {
                         [Sequelize.Op.eq]: req.body.pid,
-                    }
+                    },
                 },
             })
             .then((result) => {
@@ -115,5 +241,8 @@ routes.push({
             });
     },
 });
+
+
+
 
 module.exports = routes;
