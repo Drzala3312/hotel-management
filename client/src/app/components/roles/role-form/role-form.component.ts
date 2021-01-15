3b6f184ee74387cfe7b0a2e8a6f48559daa94520
Dhaval-Thummar <thummardhaval123@gmail.com>
2021-01-15 23:39:02 +0530

    Multi Selector

--- client/src/app/components/roles/role-form/role-form.component.ts
+++ client/src/app/components/roles/role-form/role-form.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RoleController } from '../../../ducks/roles/role.controller';
 import { PermissionController } from '../../../ducks/permissions/permission.controller';
 import _ from 'lodash';
 import { ActivatedRoute, Router } from '@angular/router';
-import { IDropdownSettings } from 'ng-multiselect-dropdown';
+//import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-role-form',
@@ -11,63 +11,61 @@ import { IDropdownSettings } from 'ng-multiselect-dropdown';
   styleUrls: ['./role-form.component.css']
 })
 export class RoleFormComponent implements OnInit {
-    public id: number;
-    dropdownList = [];
-    selectedItems = [];
-    dropdownSettings :IDropdownSettings;
-    isNoData = false;
-    public availablePermissions: any[];
-    public selectedPermissions: any [];
-    public form: any = {
-        rolename: null,
-        permission: null
-    };
+  public id: number;
+  dropdownList = [];
+  //dropdownSettings :IDropdownSettings;
+  public dropdownSettings: any = {
+    singleSelection: false,
+    text: 'Select Permissions',
+    selectAllText: 'Select All',
+    unSelectAllText: 'UnSelect All',
+    enableSearchFilter: true,
+    classes: 'multiselect'
+  };
+  isNoData = false;
+  public availablePermissions: any[];
+  public selectedPermissions: any[];
+  public form: any = {
+    rolename: null,
+    permission: null
+  };
   constructor(
     private route: ActivatedRoute,
     private _router: Router,
-     private pc:PermissionController,
-     private rc:RoleController,
+    private pc: PermissionController,
+    private rc: RoleController,
   ) {
+    this.pc.getPermissions().subscribe((permission) => {
+      this.isNoData = permission ? (permission.length === 0 ? true : false) : false;
+      if (permission.length > 0) {
+        this.availablePermissions = _.map(permission, (item) => {
+          return {
+            id: item.pid,
+            itemName: item.pname
+          };
+        });
+      }
+    }, (error: any) => {
+      console.log(error);
+    })
 
-   }
+  }
 
   ngOnInit(): void {
 
-    this.dropdownSettings={
-        singleSelection: false,
-        idField: 'item_id',
-        textField: 'item_text',
-        selectAllText: 'Select All',
-        unSelectAllText: 'UnSelect All',
-        itemsShowLimit: 3,
-        allowSearchFilter: true
-      };
-    this.pc.getPermissions().subscribe((permission) => {
-        this.isNoData = permission ? (permission.length === 0 ? true : false) : false;
-        if (permission.length > 0) {
-            this.availablePermissions = _.map(permission, (item) => {
-                return {
-                    id: item.pid,
-                    itemName: item.pname
-                };
-            });
-
-            console.log(this.availablePermissions);
-        }
-    }, (error: any) => {
-        console.log(error);
-    })
+    // this.dropdownSettings={
+    //     singleSelection: false,
+    //     idField: 'item_id',
+    //     textField: 'item_text',
+    //     selectAllText: 'Select All',
+    //     unSelectAllText: 'UnSelect All',
+    //     itemsShowLimit: 3,
+    //     allowSearchFilter: true
+    //   };
 
 
   }
-
-  onItemSelect(item: any) {
-    console.log(item);
-  }
-  onSelectAll(items: any) {
-    console.log(items);
-  }
-  onSubmit(item: any){
-
+  onSubmit(item: any) {
+    console.log(this.selectedPermissions);
   }
 }
